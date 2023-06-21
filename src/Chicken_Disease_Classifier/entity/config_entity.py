2344cd31baa10d9842d30758b5d189a9663413c6
Pratik94229 <pratik.941@gmail.com>
2023-06-21 22:40:34 +0530

    user app and prediction pipeline created

--- src/Chicken_Disease_Classifier/entity/config_entity.py
+++ src/Chicken_Disease_Classifier/entity/config_entity.py
@@ -35,7 +35,6 @@ class TrainingConfig:
     root_dir: Path
     trained_model_path: Path
     updated_base_model_path: Path
-    training_data: Path
     params_epochs: int
     params_batch_size: int
     params_is_augmentation: bool
